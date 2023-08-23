b93618051568a6054b4d6e0c26c05992feb29ea7
“Muzyria” <fila090580@gmail.com>
2023-08-23 17:30:10 +0300

    new

--- debug.py
+++ debug.py
@@ -1,14 +1,16 @@
 from pywinauto import Application
 import time
 
-app = Application(backend="uia").start(fr"calc.exe", timeout=10)
+# app = Application(backend="uia").start(fr"calc.exe", timeout=10)
+app = Application(backend="win32").start(fr"calc.exe", timeout=10)
 
 
 app.connect(best_match="Calculator", timeout=5)
 
 time.sleep(2)
 
-numbut = app["Calculator"].child_window(auto_id="num5Button")
+# numbut = app["Calculator"].child_window(auto_id="num5Button")
+numbut = app.top_window().child_window(Name="Calculator")
 
 numbut.draw_outline()
 
