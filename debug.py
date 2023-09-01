5b1f14047f1db585661bf987be1e52a36491b09f
“Muzyria” <fila090580@gmail.com>
2023-09-01 13:27:41 +0300

    new

--- debug.py
+++ debug.py
@@ -1,6 +1,14 @@
 from pywinauto import Application
 import time
 
+
+NAVIGATION_BUTTON = "TogglePaneButton"
+SETTING_ITEM = "SettingsItem"
+STANDARD_ITEM = "Standard"
+PLUS_BUTTON = "plusButton"
+EQUAL_BUTTON = "equalButton"
+RESULT = "CalculatorResults"
+
 # app = Application(backend="uia").start(fr"calc.exe", timeout=10)
 app = Application(backend="uia").start(fr"calc.exe", timeout=10)
 
@@ -10,10 +18,16 @@ app.connect(best_match="Calculator", timeout=5)
 time.sleep(2)
 
 # numbut = app["Calculator"].child_window(auto_id="num5Button")
-numbut = app.top_window()
+app["Calculator"].child_window(auto_id=NAVIGATION_BUTTON).set_focus().click()
+app["Calculator"].child_window(auto_id=SETTING_ITEM).set_focus().click_input()
+# numbut.draw_outline()
+
+
+
+
 # print(numbut.print_control_identifiers())
 
-numbut.draw_outline()
+
 
 
 time.sleep(5)
